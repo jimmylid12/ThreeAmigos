7401f0fe85ffa78769352e44a4b72fb86d05db6c
jimmylid12 <jameslid12@msn.com>
2019-11-04 17:16:12 +0000

    uploading new project

---
+++ ThreeAmigos/Controllers/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ThreeAmigos.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+
+        // GET: /Register
+
+         
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+        // GET /register/Welcome
+
+        public string Welcome()
+        {
+            return "testing 1 2 3";
+        }
+    }
+}
